9b7b17209c7136648dd5e5d2c24dde8a08051910
SAGAR KUMAR JHA <jha8768@gmail.com>
2023-12-17 00:26:26 +0530

    Knapsack variation

--- DP/DP_ON_SUBSEQUENCES/KNAPSACK_VARIATION/CoinChange2.cpp
+++ DP/DP_ON_SUBSEQUENCES/KNAPSACK_VARIATION/CoinChange2.cpp
@@ -1,2 +1,46 @@
 // LEETCODE 518
 
+// Same as count subset with sum equal to target
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=amount;j++){
+                if(coins[i-1]<=j){
+                    dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
+                } else {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount];
+    }
+};
+
+
+//Space optimiside dp
+int count(int coins[], int n, int sum)
+{
+    // table[i] will be storing the number of solutions for
+    // value i. We need sum+1 rows as the dp is
+    // constructed in bottom up manner using the base case
+    // (sum = 0)
+    int dp[sum + 1];
+ 
+    // Initialize all table values as 0
+    memset(dp, 0, sizeof(dp));
+ 
+    // Base case (If given value is 0)
+    dp[0] = 1;
+ 
+    // Pick all coins one by one and update the table[]
+    // values after the index greater than or equal to the
+    // value of the picked coin
+    for (int i = 0; i < n; i++)
+        for (int j = coins[i]; j <= sum; j++)
+            dp[j] += dp[j - coins[i]];
+    return dp[sum];
+}
