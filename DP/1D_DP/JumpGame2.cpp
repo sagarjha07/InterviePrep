51fdff87287308c35072583e2b10634ba27827a6
SAGAR KUMAR JHA <jha8768@gmail.com>
2023-12-16 16:32:27 +0530

    1D DP VARIATIONS

--- DP/1D_DP/JumpGame2.cpp
+++ DP/1D_DP/JumpGame2.cpp
@@ -1,1 +1,27 @@
 // LEETCODE 45
+
+/*
+if(i==positionn it has reached){
+    jumps++
+    pos=maxPositionItcanReach
+}
+*/
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int maxPositionCanReach=0;
+        int jumps=0;
+        int pos=0;
+        for(int i=0;i<n;i++){
+            maxPositionCanReach= max(maxPositionCanReach,i+nums[i]);
+            if(i==n-1) return jumps;
+            if(i==pos){
+                jumps++;
+                pos=maxPositionCanReach;
+            }
+        }
+        return jumps;
+    }
+};
