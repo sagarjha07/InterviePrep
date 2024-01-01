0ba709d2ab326169b9150c621db559404eae7e22
SAGAR KUMAR JHA <jha8768@gmail.com>
2024-01-01 20:36:26 +0530

    Greedy Problems

---
+++ Greedy/HandOfStraights.cpp
@@ -0,0 +1,21 @@
+// LEETCODE 846
+
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& arr, int k) {
+        int n = arr.size();
+        if(n%k!=0) return false;
+        map<int,int> mp;
+        int numOFGroups=n/k;
+        for(int i=0;i<arr.size();i++) mp[arr[i]]+=1;
+        for(int i=0;i<numOFGroups;i++){
+            int temp = (*mp.begin()).first;
+            for(int j=temp;j<temp+k;j++){
+                if(mp.find(j)==mp.end()) return false;
+                mp[j]-=1;
+                if(mp[j]==0) mp.erase(j);
+            }
+        }
+        return true;
+    }
+};
