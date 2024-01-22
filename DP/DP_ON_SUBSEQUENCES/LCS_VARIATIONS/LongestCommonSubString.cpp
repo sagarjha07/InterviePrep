ef222763057d03f522c95781f055abf51104845e
Sagar Kumar Jha <57954923+sagarjha07@users.noreply.github.com>
2024-01-23 02:36:16 +0530

    Update LongestCommonSubString.cpp

--- DP/DP_ON_SUBSEQUENCES/LCS_VARIATIONS/LongestCommonSubString.cpp
+++ DP/DP_ON_SUBSEQUENCES/LCS_VARIATIONS/LongestCommonSubString.cpp
@@ -1,22 +1,29 @@
-//SPACE OPTIMISED DP
-class Solution {
-public:
-    int longestCommonSubsequence(string text1, string text2) {
-    int a=text1.size();
-    int b=text2.size();
-    int dp[a+1][b+1];
-    for(int i=0;i<a+1;i++) dp[i][0]=0;
-    for(int i=0;i<b+1;i++) dp[0][i]=0;
-    for(int i=1;i<a+1;i++){
-        for(int j=1;j<b+1;j++){
-            if(text1[i-1]==text2[j-1]){
-                dp[i][j]=1+dp[i-1][j-1];;
-            }
-            else {
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+int LongestCommonSubstr(string X, string Y) {
+        
+        int m=X.length();
+        int n=Y.length();
+        int dp[m+1][n+1];
+        
+        // initialization
+        for(int i=0;i<=m;i++)
+            dp[i][0]=0;   // Eg LCS of "abc" & "" = 0
+        for(int j=0;j<=n;j++)
+            dp[0][j]=0;   // Eg LCS of "" & "abc" = 0
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(X[i-1]==Y[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=0;                   // ONLY THIS CHANGE
             }
         }
-    }
-    return dp[a][b];
-    }
-};
+    	 int maxLen=0;              // Now finding the max element
+         for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+               maxLen=max(maxLen,dp[i][j]);
+        }
+        return maxLen;
+		}
