f93daed3b86450147875b5f94490e52db71d0b15
SAGAR KUMAR JHA <jha8768@gmail.com>
2024-02-06 14:59:53 +0530

    imp tree problems

---
+++ Trees/CreateBinaryTreeFromParentArray.cpp
@@ -0,0 +1,24 @@
+
+class Solution{
+  public:
+    //Function to construct binary tree from parent array.
+    Node *createTree(int parent[], int n)
+    {
+        unordered_map<int,Node*> mp;
+        for(int i=0;i<n;i++){
+            mp[i]=new Node(i);
+        }
+        Node*root=NULL;
+        for(int i=0;i<n;i++){
+            if(parent[i]==-1) root = mp[i];
+            else {
+                if(mp[parent[i]]->left==NULL){
+                    mp[parent[i]]->left=mp[i];
+                } else {
+                    mp[parent[i]]->right=mp[i];
+                }
+            }
+        }
+        return root;
+    }
+};
