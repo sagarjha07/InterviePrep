288ca03a77c9e4d714a4d70e803353f273c878bf
SAGAR KUMAR JHA <jha8768@gmail.com>
2024-02-06 16:29:04 +0530

    Imp problem

--- Trees/SubTreeOfAnotherTree.cpp
+++ Trees/SubTreeOfAnotherTree.cpp
@@ -19,3 +19,71 @@ public:
         return (left || right);
     }
 };
+
+
+
+
+// GFG BFS APPROACH
+
+class Solution
+{
+    public:
+    //Function to serialize a tree and return a list containing nodes of tree.
+    vector<int> serialize(Node *root) 
+    {
+        vector<int> ans;
+        if(root==NULL) return ans;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            Node* f = q.front();
+            q.pop();
+            if(f==NULL) {
+                ans.push_back(-1);
+            }
+            else {
+                ans.push_back(f->data);
+                q.push(f->left);
+                q.push(f->right);
+            }
+        }
+        return ans;
+    }
+    
+    //Function to deserialize a list and construct the tree.
+    Node * deSerialize(vector<int> &v)
+    {
+       if(v.size()==0) return NULL;
+       int ind =0;
+       Node* root= new Node(v[0]);
+       queue<Node*> q;
+       q.push(root);
+       ind++;
+       while(!q.empty()){
+           Node* f= q.front();
+           q.pop();
+           if(v[ind]==-1) {
+               ind++;
+               f->left=NULL;
+           }
+           else if(v[ind]!=-1){
+               Node* l = new Node(v[ind]);
+               f->left =l;
+               q.push(l);
+               ind++;
+           }
+           if(v[ind]==-1) {
+               ind++;
+               f->right=NULL;
+           }
+           else if(v[ind]!=-1){
+               Node* r = new Node(v[ind]);
+               f->right = r;
+               q.push(r);
+               ind++;
+           }
+       }
+       return root;
+    }
+
+}
