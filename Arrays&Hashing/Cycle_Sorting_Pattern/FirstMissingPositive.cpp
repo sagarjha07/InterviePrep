f3f1d16bfca1e825c0baaef55d853fde53fe8064
SAGAR KUMAR JHA <jha8768@gmail.com>
2024-02-12 22:06:02 +0530

    CYCLE SORT PATTERN PROBLEMS

---
+++ Arrays&Hashing/Cycle_Sorting_Pattern/FirstMissingPositive.cpp
@@ -0,0 +1,22 @@
+// LEETCODE 41
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        // CYCLE SORT
+        while (i < n) {
+            if (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            } else {
+                i++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+        return n + 1;
+    }
+};
