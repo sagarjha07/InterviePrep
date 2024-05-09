f8f311c5bb99c7beba7931a1a4e98bd66bce04a3
Sagar Kumar Jha <57954923+sagarjha07@users.noreply.github.com>
2024-05-09 22:18:34 +0530

    Update TargetSum.cpp

--- DP/DP_ON_SUBSEQUENCES/KNAPSACK_VARIATION/TargetSum.cpp
+++ DP/DP_ON_SUBSEQUENCES/KNAPSACK_VARIATION/TargetSum.cpp
@@ -24,3 +24,33 @@ public:
 		return dp[n][sum];
     }
 };
+
+
+// SPACE OPTIMISED DP
+
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& arr, int target) {
+        int n=arr.size();
+        int sum=0;
+        for(int i:arr) sum+=i;
+        sum+=target;
+        if(sum&1) return 0;
+        sum=sum/2;
+        if(sum<0) return 0;
+        vector<int> prev(sum+1,0);
+        vector<int> curr(sum+1,0);
+        prev[0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(arr[i-1]<=j){
+                    curr[j]=prev[j]+prev[j-arr[i-1]];
+                } else {
+                    curr[j]=prev[j];
+                }
+            }
+            prev=curr;
+        }
+        return prev[sum];
+    }
+};
