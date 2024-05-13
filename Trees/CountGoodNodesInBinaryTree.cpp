b49550883738d2056fe930edb879e5e17e607b7c
Sagar Kumar Jha <57954923+sagarjha07@users.noreply.github.com>
2024-05-14 00:07:16 +0530

    Update CountGoodNodesInBinaryTree.cpp

--- Trees/CountGoodNodesInBinaryTree.cpp
+++ Trees/CountGoodNodesInBinaryTree.cpp
@@ -1,4 +1,8 @@
 // LEETCODE 1448
+// Given a binary tree root, a node X in the tree is named good 
+//if in the path from root to X there are no nodes with a value greater than X.
+
+Return the number of good nodes in the binary tree.
 
 class Solution {
 public:
