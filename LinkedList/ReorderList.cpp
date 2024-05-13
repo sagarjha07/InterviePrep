ba64193bdc61eaea3ac152ffcd3c7af9ece02384
Sagar Kumar Jha <57954923+sagarjha07@users.noreply.github.com>
2024-05-14 00:33:21 +0530

    Update ReorderList.cpp

--- LinkedList/ReorderList.cpp
+++ LinkedList/ReorderList.cpp
@@ -1,15 +1,5 @@
 //LEETCODE 143
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* reverse(ListNode*head){
