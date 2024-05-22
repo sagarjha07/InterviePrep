cd2dc4ae84dfcb61c8b1056c1c2caa57ff9a18d8
Sagar Kumar Jha <57954923+sagarjha07@users.noreply.github.com>
2024-05-22 21:55:33 +0530

    Update MergeIntervals.cpp

--- Intervals/MergeIntervals.cpp
+++ Intervals/MergeIntervals.cpp
@@ -1,5 +1,6 @@
 // lEETCODE 56
 
+// APPROACH 1
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& v) {
@@ -13,6 +14,31 @@ public:
             else{
                 ans.push_back(v[i]);
             }
+        }
+        return ans;
+    }
+};
+
+// APPROACH 2
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n= intervals.size();
+        int i=0;
+        vector<vector<int>> ans;
+        while(i<n && intervals[i][1]<newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        while(i<n && intervals[i][0]<=newInterval[1]){
+            newInterval[0] = min(intervals[i][0],newInterval[0]);
+            newInterval[1] = max(intervals[i][1],newInterval[1]);
+            i++;
+        }
+        ans.push_back(newInterval);
+        while(i<n){
+            ans.push_back(intervals[i]);
+            i++;
         }
         return ans;
     }
